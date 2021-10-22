e9fb21047c7793eee2639c0c0c1d7c68fb7b8613
Diego Perez <dperez@acumatica.com>
2021-10-21 19:20:23 -0500

    Making the index page anonymous

---
+++ Registration.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using PX.Security;
+using System;
+using System.Collections.Generic;
+
+namespace Tracumatica
+{
+    public class Registration : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+            builder.Configure<AuthenticationManagerOptions>(ConfigureAuthentication);
+        }
+
+
+
+        private void ConfigureAuthentication(AuthenticationManagerOptions options)
+        {
+            options.AddLocation("/LocationTracking/index.html")
+            .WithCookie()
+            .WithAnonymous();
+        }
+    }
+}
