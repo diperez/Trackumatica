fb26509b7aa2ef28e8c8d6886753cfdcf3b72901
Diego Perez <dperez@acumatica.com>
2021-10-21 19:26:47 -0500

    Adding code to depart on 2nd appointment when you complete the 1st one

--- FS/GraphExtensions/AppointmentEntryExt.cs
+++ FS/GraphExtensions/AppointmentEntryExt.cs
@@ -12,6 +12,70 @@ namespace Tracumatica
     {
         public static bool IsActive() => true;
 
+        // overriding this to "depart" next available appointment.
+        // depart action shows smartpanel so invoking underlying method
+        public delegate void BaseCalculateCosts();
+
+        [PXOverride]
+        public void CalculateCosts(BaseCalculateCosts BaseInvoke)
+        {
+            BaseInvoke();
+            if (Base.AppointmentRecords.Current == null) { return; }
+
+            DateTime? dtSchdDate = Base.Accessinfo.BusinessDate;
+            FSAppointmentEmployee employee = Base.AppointmentServiceEmployees.Select().First();
+
+            FSAppointment nextAppointment = PXSelectJoin<FSAppointment,
+                                    InnerJoin<FSAppointmentEmployee, On<FSAppointmentEmployee.appointmentID, Equal<FSAppointment.appointmentID>>>,
+                                    Where<FSAppointment.appointmentID, NotEqual<Required<FSAppointment.appointmentID>>,
+                                        And<FSAppointment.scheduledDateTimeBegin, GreaterEqual<Required<FSAppointment.scheduledDateTimeBegin>>,
+                                        And<FSAppointmentEmployee.employeeID, Equal<Required<FSAppointmentEmployee.employeeID>>,
+                                        And<FSAppointment.canceled, Equal<False>,
+                                        And<FSAppointment.completed, Equal<False>,
+                                        And<FSAppointment.closed, Equal<False>>>>>>>,
+                                    OrderBy<Asc<FSAppointment.scheduledDateTimeBegin>>>.
+                                    SelectWindowed(Base, 0, 1, Base.AppointmentRecords.Current.AppointmentID, dtSchdDate, employee.EmployeeID);
+
+            if (nextAppointment != null)
+            {
+                AppointmentEntry apptGraph = PXGraph.CreateInstance<AppointmentEntry>();
+                apptGraph.AppointmentRecords.Current = apptGraph.AppointmentRecords.Search<FSAppointment.refNbr>
+                                                            (nextAppointment.RefNbr, nextAppointment.SrvOrdType);
+                //apptGraph.departStaff.Press();
+                FSLogActionStartFilter filter = apptGraph.LogActionStartFilter.Current;
+                apptGraph.SetLogActionPanelDefaults(apptGraph.LogActionStartFilter.View, filter, ID.LogActions.START, FSLogActionFilter.type.Values.Travel, true);
+
+                foreach (FSAppointmentStaffDistinct row in apptGraph.LogActionStaffDistinctRecords.Select())
+                {
+                    row.Selected = true;
+                    apptGraph.LogActionStaffDistinctRecords.Update(row);
+                }
+
+
+                string travelItemLineRef = apptGraph.GetItemLineRef(Base, apptGraph.AppointmentRecords.Current.AppointmentID, true);
+
+                if (travelItemLineRef == null)
+                {
+                    FSAppointmentDet fsAppointmentDetRow = (FSAppointmentDet)apptGraph.AppointmentDetails.Cache.CreateInstance();
+                    fsAppointmentDetRow.LineType = ID.LineType_ALL.SERVICE;
+                    fsAppointmentDetRow.InventoryID = apptGraph.ServiceOrderTypeSelected.Current.DfltBillableTravelItem;
+                    fsAppointmentDetRow = apptGraph.AppointmentDetails.Insert(fsAppointmentDetRow);
+
+                    apptGraph.LogActionFilter.Current.DetLineRef = fsAppointmentDetRow.LineRef;
+                }
+                else
+                {
+                    apptGraph.LogActionFilter.Current.DetLineRef = travelItemLineRef;
+                }
+
+                apptGraph.StartTravelAction();
+                apptGraph.AppointmentRecords.Current.Status = CustomStatus.Traveling; // New Status is added via workflow. 
+                apptGraph.AppointmentRecords.UpdateCurrent();
+                apptGraph.Save.Press();
+            }
+        }
+
+
         protected virtual void _(Events.RowPersisting<FSAppointment> e)
         {
             if (e.Operation == PXDBOperation.Insert || e.Operation == PXDBOperation.Update)
